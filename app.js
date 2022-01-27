ae990e4174a40358ba1b13fe24c4529043191af0
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-01-27 15:05:14 -0500

    added mobile friendly buttons

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
 "use strict";
+
+const numAverage = function (numArr) {
+  const arrLength = numArr.length;
+  let newNum = 0;
+  for (let i = 0; i < arrLength; i++) {
+    newNum += numArr[i];
+  }
+  console.log(newNum / arrLength);
+};
+
+//numAverage([1, 4, 7]); //  Output: 4
+//numAverage([10, 5]); //Output: 7.5
+//numAverage([1.5, 3, 2.5, 1]); //Output: 2
+
+// MEDIUM: Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+//  (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+// You are given a target value to search. If found in the array return its index, otherwise return -1.
+// You may assume no duplicate exists in the array.
+// Hint:  Use a function. Use the built in method .indexOf( ) and/or  for loops. Review your lesson on Arrays to
+// aid in working through this problem.
+
+const targetArr = function (input, target) {
+  if (input.includes(target)) {
+    console.log(input.indexOf(target));
+  } else {
+    console.log(-1);
+  }
+};
+
+//targetArr([4, 5, 6, 7, 0, 1, 2], 0); //target = 0 Output: 4
+//targetArr([4, 5, 6, 7, 0, 1, 2], 3); //target = 3 Output: -1.
+
+// HARD: Create two buttons, one that when clicked turns the background of the body red.
+// The other, when clicked, turns the body's background color white.
+//  Each of the background colors should be defined as class styles and when the buttons
+//  are pushed the body's classList is updated to include the correct class for the background.
+// Create a single function that takes an input className and updates the body's
+//  classList to ONLY include that className
+// *Note: Check Canvas for the starter code needed for this exercise.
+//  The starter code will be located in a folder named hard_js.
+
+const redBtn = document.querySelector("#red");
+const whiteBtn = document.querySelector("#white");
+
+redBtn.addEventListener("mousedown", function () {
+  redBtn.style.backgroundColor = "red";
+  redBtn.addEventListener("mouseup", function () {
+    redBtn.style.backgroundColor = "black";
+  });
+});
+
+whiteBtn.addEventListener("mousedown", function () {
+  whiteBtn.style.backgroundColor = "white";
+  whiteBtn.style.color = "black";
+  whiteBtn.addEventListener("mouseup", function () {
+    whiteBtn.style.backgroundColor = "black";
+    whiteBtn.style.color = "white";
+  });
+});
+
+redBtn.addEventListener("touchstart", function () {
+  redBtn.style.backgroundColor = "red";
+  redBtn.addEventListener("touchend", function () {
+    redBtn.style.backgroundColor = "black";
+  });
+});
+whiteBtn.addEventListener("touchstart", function () {
+  whiteBtn.style.backgroundColor = "white";
+  whiteBtn.style.color = "black";
+  whiteBtn.addEventListener("touchend", function () {
+    whiteBtn.style.backgroundColor = "black";
+    whiteBtn.style.color = "white";
+  });
+});
